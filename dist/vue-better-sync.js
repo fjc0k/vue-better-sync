8d423281a73fcca0843d982363c45b33be79144c
fjc0k <fjc0kb@gmail.com>
2018-03-25 00:32:04 +0800

    build

--- dist/vue-better-sync.js
+++ dist/vue-better-sync.js
@@ -57,6 +57,7 @@ var index = (function (ref) {
       var proxy = "actual" + PropName;
       var syncMethod = "sync" + PropName;
       var directSyncMethod = "sync" + PropName + "Directly";
+      var beforeSyncMethod = "beforeSync" + PropName;
       ctx[X_DATA_PROPS][proxy] = propName;
 
       ctx.methods[directSyncMethod] = function (newValue, oldValue, propChangedBy) {
@@ -88,10 +89,24 @@ var index = (function (ref) {
       };
 
       ctx.watch[proxy] = function (newValue, oldValue) {
+        var this$1 = this;
+
         // now: this[proxy] === newValue
-        if (newValue !== oldValue) {
+        if (newValue !== oldValue && newValue !== this[propName]) {
           // so: `this[proxy] = newValue` will not trigger watcher
-          this[directSyncMethod](newValue, oldValue, X_PROP_CHANGED_BY_PROXY);
+          var confirm = function () {
+            this$1[directSyncMethod](newValue, oldValue, X_PROP_CHANGED_BY_PROXY);
+          };
+
+          var cancel = function () {
+            this$1[proxy] = oldValue;
+          };
+
+          if (typeof this[beforeSyncMethod] === 'function') {
+            this[beforeSyncMethod](oldValue, newValue, confirm, cancel);
+          } else {
+            confirm();
+          }
         }
       };
     });
